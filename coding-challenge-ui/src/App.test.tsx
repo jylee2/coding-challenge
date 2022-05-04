31efbca5ab9202663cd5cd7839b1a596d16abde4
jylee2 <leej1025y@live.com>
2022-05-04 12:06:53 +0800

    build: setup

--- coding-challenge-ui/src/App.test.tsx
+++ coding-challenge-ui/src/App.test.tsx
@@ -1,15 +1,9 @@
-import App from './App';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test('renders app header', () => {
-  const { getByText } = render(<App />);
-  const headerText = getByText("Analytics Dashboard");
-  expect(headerText).toBeInTheDocument();
-});
-
-test('renders user name', () => {
-  const { getByText } = render(<App />);
-  const headerText = getByText("Welcome, Guest!");
-  expect(headerText).toBeInTheDocument();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
